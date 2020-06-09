17be1b68f4b50de93fb26336fb11aa1e5d9c838d
Ranaldi Andrea <andrea.ranaldi@gmail.com>
2020-06-09 14:27:49 +0200

    Eliminate duplicazioni

--- app/javascript/packs/loader.js
+++ app/javascript/packs/loader.js
@@ -1,24 +1,18 @@
-document.addEventListener('ajax:beforeSend', function(event) { 
-  event.target.classList.add('is-loading')
-  document.getElementById('loader').classList.remove('is-hidden')
-  })
-document.addEventListener('ajax:success', function(event) { 
-  event.target.classList.remove('is-loading')
-  document.getElementById('loader').classList.add('is-hidden')
-  })
+const loading = (event) => { event.target.classList.add('is-loading'); document.getElementById('loader').classList.remove('is-hidden')};
+const loaded = (event) => {  event.target.classList.remove('is-loading'); document.getElementById('loader').classList.add('is-hidden') };
+const activate = (target) => { document.getElementById(target).classList.add('is-active') };
+
+document.addEventListener('ajax:beforeSend', function(event) { loading(event) })
+document.addEventListener('ajax:success', function(event) { loaded(event) })
 document.addEventListener('ajax:error', function(event) {
-   var detail = event.detail;
-   var data = detail[0];
-   var status = detail[1];
-   var xhr = detail[2];
-   console.log(event)
-   if (status == "Unauthorized") {
-     document.getElementById('timeout').classList.add('is-active')
-     event.target.classList.remove('is-loading')
-     document.getElementById('loader').classList.add('is-hidden')
+   console.log(event);
+   if (event.detail[1] == "Unauthorized") {
+     loaded(event);
+     activate('timeout');
    } else {
-     document.getElementById('error').classList.add('is-active')
-     event.target.classList.remove('is-loading')
-     document.getElementById('loader').classList.add('is-hidden')
+     loaded(event);
+     activate('error') ;
    }
  })
+
+
