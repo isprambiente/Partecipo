9ec56da3c3d4697c7597d26f7d8cf9c273208e0f
Andrea Ranaldi <andrea.ranaldi@gmail.com>
2022-01-07 10:23:46 +0100

    Migrate to rails 7.0

--- app/models/fact.rb
+++ app/models/fact.rb
@@ -35,7 +35,11 @@
 #   @return [Array] list of [Fact] with stop_on minor than Time.zone.today, ordered by start_on desc
 class Fact < ApplicationRecord
   has_rich_text :body
-  has_one_attached :image
+  has_one_attached :image do |attachable|
+    attachable.variant :card, resize_to_limit: [417, 278]
+    attachable.variant :aside, resize_to_limit: [318, 318]
+    attachable.variant :ticket, resize_to_limit: [150, 68]
+  end
   belongs_to :group
   has_many :happenings, dependent: :destroy
   has_many :tickets, through: :happenings
