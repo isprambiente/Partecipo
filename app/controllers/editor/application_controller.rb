e1f6e9bb3f06b6496faa87a1ec336e3252755462
Andrea Ranaldi <andrea.ranaldi@gmail.com>
2022-02-09 10:49:21 +0100

    If user isn`t authenticated redirecto to sign-in

--- app/controllers/editor/application_controller.rb
+++ app/controllers/editor/application_controller.rb
@@ -8,6 +8,6 @@ class Editor::ApplicationController < ApplicationController
 
   # deny access unless current_user is an editor
   def authenticate_editor!
-    access_denied! unless current_user.editor? || current_user.admin?
+    access_denied! unless user_signed_in? && (current_user.editor? || current_user.admin?)
   end
 end
