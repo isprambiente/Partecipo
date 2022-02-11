ff61e5d4881c70f5c22257f9557a7b087c201a0e
Andrea Ranaldi <andrea.ranaldi@gmail.com>
2022-02-11 07:43:34 +0100

    Aggiunto filtro per gruppi

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,20 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 Fact.destroy_all
 Group.destroy_all
-g = Group.create title: 'test group'
+g1 = Group.create title: 'test group'
+g2 = Group.create title: 'Other group'
 facts = [
-  {title: 'evento uno', start_on: Time.zone.now, stop_on: Time.zone.now + 1.year, group: g},
-  {title: 'evento due', start_on: Time.zone.now, stop_on: Time.zone.now + 1.year, group: g}
+  {title: 'evento uno', start_on: Time.zone.now, stop_on: Time.zone.now + 1.year, group: g1},
+  {title: 'evento due', start_on: Time.zone.now, stop_on: Time.zone.now + 1.year, group: g1},
+  {title: 'evento due', start_on: Time.zone.now, stop_on: Time.zone.now + 1.year, group: g1},
+  {title: 'evento uno', start_on: Time.zone.now, stop_on: Time.zone.now + 1.year, group: g1},
+  {title: 'evento due', start_on: Time.zone.now, stop_on: Time.zone.now + 1.year, group: g1},
+  {title: 'evento due', start_on: Time.zone.now, stop_on: Time.zone.now + 1.year, group: g2},
+  {title: 'evento uno', start_on: Time.zone.now, stop_on: Time.zone.now + 1.year, group: g2},
+  {title: 'evento due', start_on: Time.zone.now, stop_on: Time.zone.now + 1.year, group: g2},
+  {title: 'evento due', start_on: Time.zone.now, stop_on: Time.zone.now + 1.year, group: g2}
 ]
+
 happenings = [
   {start_at: Time.zone.now + 1.day, start_sale_at: Time.zone.now - 2.days, stop_sale_at: Time.zone.now + 1.day, max_seats: 100, max_seats_for_ticket: 5, repeat_for: 10, repeat_in: ['0','1','2','3','4','5','6','7']},
   {start_at: Time.zone.now + 1.day + 2.hour, start_sale_at: Time.zone.now - 2.days, stop_sale_at: Time.zone.now + 1.day, max_seats: 100, max_seats_for_ticket: 5, repeat_for: 10, repeat_in: ['0','1','2','3','4','5','6','7']}
