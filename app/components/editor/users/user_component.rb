2f80bdb9cfe0abb585e92b272af24f5ef9ff09ef
MDreW <andrea.ranaldi@gmail.com>
2024-01-31 10:00:33 +0100

    moved UserComponent under editor scope

---
+++ app/components/editor/users/user_component.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+class Editor::Users::UserComponent < CommonComponent
+  def initialize(user:)
+    super
+    @user = user
+  end
+
+  def confirmed_tag
+    if @user.confirmed_at?
+      tag.div icon_text("fas fa-user", t('.confirmed')),   class: "tag is-success"
+    else
+      tag.div icon_text("fas fa-user", t('.unconfirmed')), class: "tag is-warning"
+    end
+  end
+
+  def active_tag
+    if @user.locked_at?
+      tag.div icon_text("fas fa-lock", t('.lock')), class: "tag is-danger"
+    else
+      tag.div icon_text("fas fa-unlock", t('.unlock')), class: "tag is-success"
+    end
+  end
+end
