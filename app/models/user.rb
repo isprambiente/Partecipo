f41b61941d7d2ed75946b96b4ff58153ae640117
MDreW <andrea.ranaldi@gmail.com>
2024-11-22 19:01:48 +0100

    test with model

--- app/models/user.rb
+++ app/models/user.rb
@@ -55,6 +55,17 @@ class User < ApplicationRecord
   scope :editors, -> { where editor: true }
   scope :admins, -> { where admin: true }
 
+  def self.from_omniauth(auth)
+    user = find_or_initialize_by(username: auth.uid)
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0, 20]
+    #user.name = auth.info.name   # assuming the user model has a name
+    #user.image = auth.info.image # assuming the user model has an image
+
+    user.save
+    user
+  end
+
   # @return [String] gravatar url for user
   def avatar_url
     hash = Digest::MD5.hexdigest(email)
