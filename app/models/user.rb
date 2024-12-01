2c035a084347893c536b9b283eb027c84cfaa1e3
MDreW <andrea.ranaldi@gmail.com>
2024-12-02 00:23:36 +0100

    Fixed random password for OIDC user

--- app/models/user.rb
+++ app/models/user.rb
@@ -59,7 +59,7 @@ class User < ApplicationRecord
   def self.from_omniauth(auth)
     user = find_or_initialize_by(username: auth.uid)
     user.email = auth.info.email
-    user.password = Devise.friendly_token[0, 20]
+    user.password = SecureRandom.alphanumeric(20)
     user.name = auth.info.try(ENV.fetch('RAILS_OIDC_NAME'){'given_name'})
     user.surname = auth.info(ENV.fetch('RAILS_OIDC_SURNAME'){'family_name'})
     user.skip_confirmation!
