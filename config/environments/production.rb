7e144bdf7d93707ba1266780e89924f6051a1f99
MDreW <andrea.ranaldi@gmail.com>
2024-12-06 13:24:01 +0100

    secret_key_base from environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,6 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.require_master_key = false
+  config.read_encrypted_secrets = false
 
   # Code is not reloaded between requests.
   config.enable_reloading = false
