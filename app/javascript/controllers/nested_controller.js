6a216670ee235c79a99ec4c484e80ec1067e944d
Andrea Ranaldi <andrea.ranaldi@gmail.com>
2024-12-18 17:04:57 +0100

    104 add happening question (#109)
    * Update (#108)
    
    * 103 aggiungere tipi di domande integer e data (#107)
    
    * aggiunte tipologie di domande per allegati, date, numeri
    
    * Aggiunti devcontainer
    
    * navbar title configuration with RAILS_TITLE and RAILS_ICON (#106)
    
    * attivati tasti per aggiungere e rimuovere le domande per gli happening
    
    * removed codeql external flow

--- app/javascript/controllers/nested_controller.js
+++ app/javascript/controllers/nested_controller.js
@@ -2,15 +2,23 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="nested"
 export default class extends Controller {
-  static targets = ["row"] // 1
+  static targets = [ 'content', 'template' ]
 
-  connect() {
-  }
-
-  deleteRow() { // 2
-    event.preventDefault(); // 3
-
-    let index = event.currentTarget.getAttribute("data-index"); // 4, 5
-    this.bookRowTargets[index].classList.toggle("hidden"); // 6, 7
+  add() {
+    event.preventDefault()
+    let clone = document.createElement('div')
+    clone.setAttribute('class', this.templateTarget.getAttribute('class'))
+    clone.dataset.target = 'nested.entry'
+    clone.innerHTML = this.templateTarget.innerHTML
+    this.contentTarget.appendChild(clone)
+    let index = this.templateTarget.dataset.index
+    let newIndex = this.contentTarget.children.length + 1
+    clone.querySelectorAll('[name]').forEach( (field) => { 
+      field.setAttribute('name', field.getAttribute('name').replace(`[${index}]`, `[${newIndex}]`))
+      field.setAttribute('id', field.getAttribute('name').replace(`[${index}]`, `[${newIndex}]`))
+    } )
+    clone.querySelectorAll('[for]').forEach( (field) => { 
+      field.setAttribute('for', field.getAttribute('for').replace(`_${index}_`, `_${newIndex}_`))
+    } )   
   }
 }
