2757c8f60e59a738c0eaa18381d4b9fac8c4084a
MDreW <andrea.ranaldi@gmail.com>
2025-01-03 23:21:12 +0100

    importmap requirments

---
+++ vendor/javascript/#util.inspect.js.js
@@ -0,0 +1,4 @@
+// #util.inspect.js@2.1.0 downloaded from https://ga.jspm.io/npm:@jspm/core@2.1.0/nodelibs/@empty.js
+
+var e=Object.freeze(Object.create(null));export{e as default};
+
---
+++ vendor/javascript/call-bind--callBound.js
@@ -0,0 +1,4 @@
+// call-bind/callBound@1.0.8 downloaded from https://ga.jspm.io/npm:call-bind@1.0.8/callBound.js
+
+import*as r from"get-intrinsic";import t from"./index.js";import"set-function-length";import"es-define-property";import"call-bind-apply-helpers";import"call-bind-apply-helpers/applyBind";var i=r;try{"default"in r&&(i=r.default)}catch(r){}var p={};var e=i;var n=t;var a=n(e("String.prototype.indexOf"));p=function callBoundIntrinsic(r,t){var i=e(r,!!t);return typeof i==="function"&&a(r,".prototype.")>-1?n(i):i};var o=p;export{o as default};
+
